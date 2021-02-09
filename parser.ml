d10cf4e5520a05f0dbe6234bb82814ce046e0049
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-02-09 07:30:58 +0530

    Refactor out parsing individual expressions
    We treat an input string as consisting of a series of expressions, and
    the previous model would parse them one by one and add them as it went
    along. The issue with this model was that it couldn't handle parsing a
    single expression, which can be useful in some cases. This commit
    separates the logic for parsing one expression from the logic to parse
    the whole input string, therefore allowing for more features to be
    implemented easily.
    
    This commit was created by squashing five commits. The original commit
    messages are given below.
    
    1.
    Remove pipe state
    
    2.
    Remove reliance on And lists
    
    This commit is technicallt non-functional: it won't pass tests, if
    there were any.
    
    3.
    Return last processed n in parsing single expression
    
    4.
    Rename _parse to _parse_one_expr
    
    5.
    Add governor

--- parser.ml
+++ parser.ml
@@ -34,8 +34,6 @@ type parser_state =
   | State6
   (* Passed , in a quantifier def. *)
   | State7
-  (* Passed | in State 1. *)
-  | State8
 
 let unexpected_error = Printf.sprintf "Parse failure: Unexpected `%c' at col %d"
 
@@ -44,22 +42,19 @@ let out_of_range_error =
 
 let numlen n = n |> string_of_int |> String.length
 
-let rec _parse inp n state expr =
+let rec _parse_one_expr inp n state expr =
   let len = String.length inp in
   if len = n then
     match state with
     (* While State2 is expecting a quantifier, a lack of a quantifier never
      * hurt anyone. *)
     | State1 | State2 ->
-        expr
+        (expr, n)
     | State3 | State4 | State5 ->
         failwith "Unexpected EOF, expecting `]'."
     (* TODO: Implement conversion to literal if this happens. *)
     | State6 | State7 ->
         failwith "Unexpected EOF, expecting `}'."
-    (* TODO: Implement conversion to literal in this case. *)
-    | State8 ->
-        failwith "Unexpected EOF, expecting any character."
   else
     let func =
       match state with
@@ -80,75 +75,61 @@ let rec _parse inp n state expr =
     in
     func inp n expr
 
-and _parse_state_1 inp n = function
-  | And lst -> (
-      let current_char = inp.[n] in
-      match current_char with
-      | '*' | '+' | '?' ->
-          failwith @@ unexpected_error current_char (n + 1)
-      | '.' ->
-          _parse inp (n + 1) State2 (And (Dot :: lst))
-      | '[' ->
-          _parse inp (n + 1) State3 (And (Or [] :: lst))
-      | a ->
-          _parse inp (n + 1) State2 (And (Character a :: lst))
-    )
-  | _ ->
-      failwith "internal failure in state 1."
+and _parse_state_1 inp n expr =
+  let current_char = inp.[n] in
+  match current_char with
+  | '*' | '+' | '?' ->
+      failwith @@ unexpected_error current_char (n + 1)
+  | '.' ->
+      _parse_one_expr inp (n + 1) State2 Dot
+  | '[' ->
+      _parse_one_expr inp (n + 1) State3 (Or [])
+  | a ->
+      _parse_one_expr inp (n + 1) State2 (Character a)
 
 and _parse_state_2 inp n expr =
-  match expr with
-  | And (h :: t) -> (
-      let current_char = inp.[n] in
-      match current_char with
-      | '?' ->
-          let new_h = BoundedRange (h, 0, 1) in
-          _parse inp (n + 1) State1 (And (new_h :: t))
-      | '*' ->
-          let new_h = UnboundedRange (h, 0) in
-          _parse inp (n + 1) State1 (And (new_h :: t))
-      | '+' ->
-          let new_h = UnboundedRange (h, 1) in
-          _parse inp (n + 1) State1 (And (new_h :: t))
-      | '{' ->
-          _parse inp (n + 1) State6 expr
-      | _ ->
-          _parse inp n State1 expr
-    )
+  let current_char = inp.[n] in
+  match current_char with
+  | '?' ->
+      let new_expr = BoundedRange (expr, 0, 1) in
+      (new_expr, n + 1)
+  | '*' ->
+      let new_expr = UnboundedRange (expr, 0) in
+      (new_expr, n + 1)
+  | '+' ->
+      let new_expr = UnboundedRange (expr, 1) in
+      (new_expr, n + 1)
+  | '{' ->
+      _parse_one_expr inp (n + 1) State6 expr
   | _ ->
-      failwith "internal failure in state 2."
+      (expr, n)
 
 and _parse_state_3 inp n expr =
-  match expr with
-  | And (h :: t) -> (
-      let current_char = inp.[n] in
-      match current_char with
-      | ']' ->
-          _parse inp (n + 1) State2 expr
-      (* A leading - is also treated as a literal. *)
-      | a -> (
-        match h with
-        | Or lst ->
-            let new_h = Or (Character a :: lst) in
-            _parse inp (n + 1) State4 (And (new_h :: t))
-        | _ ->
-            failwith "internal failure in state 3: no OR list."
-      )
-    )
-  | _ ->
-      failwith "internal failure in state 3: no AND list."
+  let current_char = inp.[n] in
+  match current_char with
+  | ']' ->
+      _parse_one_expr inp (n + 1) State2 expr
+  (* A leading - is also treated as a literal. *)
+  | a -> (
+    match expr with
+    | Or lst ->
+        let new_expr = Or (Character a :: lst) in
+        _parse_one_expr inp (n + 1) State4 new_expr
+    | _ ->
+        failwith "internal failure in state 3: no OR list."
+  )
 
 and _parse_state_4 inp n expr =
   let current_char = inp.[n] in
   match current_char with
   | '-' ->
-      _parse inp (n + 1) State5 expr
+      _parse_one_expr inp (n + 1) State5 expr
   | _ ->
-      _parse inp n State3 expr
+      _parse_one_expr inp n State3 expr
 
 and _parse_state_5 inp n expr =
   match expr with
-  | And (Or (Character c :: tl1) :: tl2) -> (
+  | Or (Character c :: tl1) -> (
       let current_char = inp.[n] in
       match current_char with
       (* As was the case with a leading -, a trailing - also seems to be a bit
@@ -174,124 +155,105 @@ and _parse_state_5 inp n expr =
              * matters in an And list, since we need to output a string that matches 
              * the ordering of the input regex. However, in this case it doesn't make
              * a difference, since the output is an Or expression. *)
-            let new_h = Or (add_chars init diff tl1) in
-            _parse inp (n + 1) State3 (And (new_h :: tl2))
+            let new_expr = Or (add_chars init diff tl1) in
+            _parse_one_expr inp (n + 1) State3 new_expr
     )
   | _ ->
-      failwith "internal error in state 5."
+      failwith "internal failure in state 5: no OR list."
 
 and _parse_state_6 inp n expr =
-  match expr with
-  | And (hd :: tl) -> (
-      let current_char = inp.[n] in
-      match current_char with
-      | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' ->
-          let quant = int_of_char current_char - 48 (* 48 is ascii for 0. *) in
-          let new_h =
-            match hd with
-            | Multiple (mexpr, prev_quant) ->
-                let new_quant = (prev_quant * 10) + quant in
-                Multiple (mexpr, new_quant)
-            | _ ->
-                Multiple (hd, quant)
-          in
-          _parse inp (n + 1) State6 (And (new_h :: tl))
-      | '}' -> (
-        match hd with
-        | Multiple (_, _) ->
-            _parse inp (n + 1) State1 expr
-        (* There is no quantifier, so the previous { has to be treated as a 
-         * literal. *)
+  let current_char = inp.[n] in
+  match current_char with
+  | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' ->
+      let quant = int_of_char current_char - 48 (* 48 is ascii for 0. *) in
+      let new_expr =
+        match expr with
+        | Multiple (mexpr, prev_quant) ->
+            let new_quant = (prev_quant * 10) + quant in
+            Multiple (mexpr, new_quant)
         | _ ->
-            _parse inp (n - 1) State1 expr
-      )
-      | ',' -> (
-        match hd with
-        | Multiple (_, _) ->
-            _parse inp (n + 1) State7 expr
-        (* Leading ',', has to be treated as a literal. *)
-        | _ ->
-            _parse inp (n - 1) State1 expr
-      )
-      | _ -> (
-        match hd with
-        | Multiple (mexpr, quant) ->
-            (* We've run into a non-digit while parsing a quantifier.
-             * This means the {} are literals, so we need to go back to the position
-             * of the { in State1 so it can be processed as such. *)
-            let num_chars = numlen quant in
-            _parse inp (n - num_chars - 1) State1 (And (mexpr :: tl))
-        | _ ->
-            _parse inp (n - 1) State1 expr
-      )
-    )
-  | _ ->
-      failwith "internal error in state 6."
+            Multiple (expr, quant)
+      in
+      _parse_one_expr inp (n + 1) State6 new_expr
+  | '}' -> (
+    match expr with
+    | Multiple (_, _) ->
+        (expr, n + 1)
+    (* There is no quantifier, so the previous { has to be treated as a
+     * literal. *)
+    | _ ->
+        (expr, n - 1)
+  )
+  | ',' -> (
+    match expr with
+    | Multiple (_, _) ->
+        _parse_one_expr inp (n + 1) State7 expr
+    (* A leading ',', has to be treated as a literal. *)
+    | _ ->
+        (expr, n - 1)
+  )
+  | _ -> (
+    match expr with
+    | Multiple (mexpr, quant) ->
+        (* We've run into a non-digit while parsing a quantifier.
+         * This means the {} are literals, so we need to go back to the position
+         * of the { in State1 so it can be processed as such. *)
+        let num_chars = numlen quant in
+        (mexpr, n - num_chars - 1)
+    | _ ->
+        (expr, n - 1)
+  )
 
 and _parse_state_7 inp n expr =
-  match expr with
-  | And (hd :: tl) -> (
-      let current_char = inp.[n] in
-      match current_char with
-      | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' ->
-          let quant = int_of_char current_char - 48 in
-          let new_h =
-            match hd with
-            | Multiple (mexpr, start_quant) ->
-                BoundedRange (mexpr, start_quant, quant)
-            | BoundedRange (brexpr, start_quant, prev_end_quant) ->
-                let new_end_quant = (prev_end_quant * 10) + quant in
-                BoundedRange (brexpr, start_quant, new_end_quant)
-            | _ ->
-                failwith "internal error in state 7: no BoundedRange."
-          in
-          _parse inp (n + 1) State7 (And (new_h :: tl))
-      | '}' -> (
-        match hd with
+  let current_char = inp.[n] in
+  match current_char with
+  | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' ->
+      let quant = int_of_char current_char - 48 in
+      let new_expr =
+        match expr with
         | Multiple (mexpr, start_quant) ->
-            let new_h = UnboundedRange (mexpr, start_quant) in
-            _parse inp (n + 1) State1 (And (new_h :: tl))
-        | BoundedRange (_, start_quant, end_quant) ->
-            if start_quant <= end_quant then _parse inp (n + 1) State1 expr
-            else failwith @@ out_of_range_error (n + 1)
-        | _ ->
-            failwith "internal error in state 7: no BoundedRange"
-      )
-      | _ -> (
-        match hd with
-        (* We can't reuse the State6 parsing because there's an additional comma. *)
-        | Multiple (mexpr, quant) ->
-            let num_chars = numlen quant in
-            _parse inp (n - num_chars - 2) State1 (And (mexpr :: tl))
-        | BoundedRange (brexpr, start_quant, end_quant) ->
-            let start_chars = numlen start_quant in
-            let end_chars = numlen end_quant in
-            _parse inp
-              (n - start_chars - end_chars - 2)
-              State1
-              (And (brexpr :: tl))
+            BoundedRange (mexpr, start_quant, quant)
+        | BoundedRange (brexpr, start_quant, prev_end_quant) ->
+            let new_end_quant = (prev_end_quant * 10) + quant in
+            BoundedRange (brexpr, start_quant, new_end_quant)
         | _ ->
             failwith "internal error in state 7: no BoundedRange."
-      )
-    )
-  | _ ->
-      failwith "internal error in state 7: no And list."
+      in
+      _parse_one_expr inp (n + 1) State7 new_expr
+  | '}' -> (
+    match expr with
+    | Multiple (mexpr, start_quant) ->
+        let new_expr = UnboundedRange (mexpr, start_quant) in
+        (new_expr, n + 1)
+    | BoundedRange (_, start_quant, end_quant) ->
+        if start_quant <= end_quant then (expr, n + 1)
+        else failwith @@ out_of_range_error (n + 1)
+    | _ ->
+        failwith "internal error in state 7: no BoundedRange"
+  )
+  | _ -> (
+    match expr with
+    (* We can't reuse the State6 parsing because there's an additional comma. *)
+    | Multiple (mexpr, quant) ->
+        let num_chars = numlen quant in
+        (mexpr, n - num_chars - 2)
+    | BoundedRange (brexpr, start_quant, end_quant) ->
+        let start_chars = numlen start_quant in
+        let end_chars = numlen end_quant in
+        (brexpr, n - start_chars - end_chars - 2)
+    | _ ->
+        failwith "internal error in state 7: no BoundedRange."
+  )
 
-and _parse_state_8 inp n expr =
-  match expr with
-  | And (Or lst :: tl) -> (
-      let current_char = inp.[n] in
-      match current_char with
-      | '*' | '+' | '?' ->
-          failwith @@ unexpected_error current_char (n + 1)
-      | '.' ->
-          _parse inp (n + 1) State2 (And (Dot :: lst))
-      | '[' ->
-          _parse inp (n + 1) State3 (And (Or [] :: lst))
-      | a ->
-          _parse inp (n + 1) State1 (And (Character a :: lst))
-    )
-  | _ ->
-      failwith "internal error in state 8"
+let rec _parse inp n expr =
+  let len = String.length inp in
+  if n >= len then expr
+  else (
+    match expr with
+    | And lst ->
+      let next_expr, next_n = _parse_one_expr inp n State1 Empty in
+      _parse inp next_n (And (next_expr::lst))
+    | _ -> failwith "internal failure in governor: no AND list."
+  )
 
-let parse (inp : string) : expr = _parse inp 0 State1 (And [])
+let parse (inp : string) : expr = _parse inp 0 (And [])
