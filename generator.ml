48fc08e808675071807f868a82199fb25c139784
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-05-12 13:23:46 +0530

    More extraction and some style changes

--- generator.ml
+++ generator.ml
@@ -1,12 +1,18 @@
-open Parser
+open Ast
 
 let rec generate_expr = function
-  | Empty -> generate_empty ()
-  | Dot -> generate_dot ()
-  | Character a -> generate_char a
-  | Or lst -> generate_or lst
-  | And lst -> generate_and lst
-  | Multiple (expr, quant) -> generate_multiple expr quant
+  | Empty ->
+      generate_empty ()
+  | Dot ->
+      generate_dot ()
+  | Character a ->
+      generate_char a
+  | Or lst ->
+      generate_or lst
+  | And lst ->
+      generate_and lst
+  | Multiple (expr, quant) ->
+      generate_multiple expr quant
   | BoundedRange (expr, start_quant, end_quant) ->
       generate_bounded_range expr start_quant end_quant
   | UnboundedRange (expr, start_quant) ->
@@ -15,17 +21,17 @@ let rec generate_expr = function
 and generate_empty () = ""
 
 and generate_dot () =
-  let randint = Random.int 95 in
-  let generated_char = char_of_int (randint + 32) in
+  let generated_char = Random.int 95 + 32 |> char_of_int in
   generate_expr (Character generated_char)
 
-and generate_char a = String.make 1 a
+and generate_char = String.make 1
 
 and generate_or lst =
-  lst |> List.length |> Random.int |> List.nth lst |> generate_expr
+  let ind = lst |> List.length |> Random.int in
+  ind |> List.nth lst |> generate_expr
 
 and generate_and lst =
-  (* It is completely possible to use List.fold_left with the ^ operator 
+  (* It is completely possible to use List.fold_left with the ^ operator
    * instead, but String.concat is much faster than the ^ operator. *)
   lst |> List.rev_map generate_expr |> String.concat ""
 
@@ -36,7 +42,9 @@ and generate_multiple expr quant =
   expr |> _gen quant [] |> String.concat ""
 
 and generate_bounded_range expr start_quant end_quant =
-  let _end_quant = if start_quant = 0 then end_quant + 1 else end_quant in
+  let _end_quant =
+    if start_quant = 0 then end_quant + 1 else end_quant - start_quant
+  in
   let quant = start_quant + Random.int _end_quant in
   generate_expr (Multiple (expr, quant))
 
@@ -44,4 +52,4 @@ and generate_unbounded_range expr start_quant =
   (* For practical reasons, I'm limiting unbounded ranges to 255 characters. *)
   generate_expr (BoundedRange (expr, start_quant, 255))
 
-let generate str = str |> Parser.parse |> generate_expr
+let generate = generate_expr
--- parser.mli
+++ parser.mli
@@ -1,12 +1,2 @@
-type expr =
-  | Empty
-  | Dot
-  | Character of char
-  | Or of expr list
-  | And of expr list
-  | Multiple of expr * int
-  | BoundedRange of expr * int * int
-  | UnboundedRange of expr * int
-
 (* [parse regexp] is the compiled form of [regexp]. *)
-val parse : string -> expr
+val parse : string -> Ast.expr
